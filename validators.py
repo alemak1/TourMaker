73fb7b81af9b7aa2895be7ce937db71b8fef4cac
yongrenwang <czie.alexmak@gmail.com>
2017-01-03 16:57:46 +0800

    Added a note the validators.py file

--- validators.py
+++ validators.py
@@ -1,2 +1,5 @@
 #A series of function that provide validation for the names of cities and states
-#passed as arguments to the initializers for our tour_maker class.
+#passed as arguments to the initializers for our tour_maker class. 
+
+#When preconditions and postconditions enforced by these validators are violated,
+#an error is raised.
