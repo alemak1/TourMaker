ad6e05d1294fed35ea311ce60ce26288d3b8e62f
yongrenwang <czie.alexmak@gmail.com>
2017-01-03 16:57:23 +0800

    Added a note to the exceptions.py module

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,7 @@
 #A set of user-defined exceptions that will be raised to enforce preconditions
 #for certains functions in the tour_maker classes, as well as to enforce 
 #validation for function parameters and return values.
+
+#Suggestion: Create a set of possible state abbreviations.  Test for membership in
+#set to ensure that the correct state abbreviations are added. Provide user with
+#a list of valid state names.
