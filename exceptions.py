3b034069edfb23b2325a4bf9d62c20b8d04c9d3e
yongrenwang <czie.alexmak@gmail.com>
2017-01-03 16:50:30 +0800

    Added a summary for our validators module

---
+++ exceptions.py
@@ -0,0 +1,3 @@
+#A set of user-defined exceptions that will be raised to enforce preconditions
+#for certains functions in the tour_maker classes, as well as to enforce 
+#validation for function parameters and return values.
---
+++ validators.py
@@ -0,0 +1,2 @@
+#A series of function that provide validation for the names of cities and states
+#passed as arguments to the initializers for our tour_maker class.
