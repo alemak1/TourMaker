b99ac566f0790e55178a2c42b119f11cd5399fc8
yongrenwang <czie.alexmak@gmail.com>
2017-01-05 10:25:20 +0800

    Added doctests and other annotations to the overloaded operators; documented other class methods and refactored code to ensure that extraneous whitespace doesn't affect equality/inequality of tour stops

--- tours.py
+++ tours.py
@@ -1,4 +1,5 @@
 from sets import Set, ImmutableSet
+import statistics
 
 #TODO ITEMS:
 	#Define error classes and implement error handling and validation in code
@@ -35,9 +36,23 @@ class Tour:
 		self.total_distance = self.get_total_distance()
 
 
+	def get_locations(self):
+		'''
+		Returns the list of tour locations
+
+		>>> Tour('New York, NY','Buffalo,NY').get_locations()
+		['New York, NY', 'Buffalo,NY']
+
+		:return: List of tour stop locations stored in current tour object
+		'''
+		return self.locations
 
 	#Helper Methods
 	def reset_request_URL(self):
+		'''Resets the current API-request URL to the base URL
+
+		:return: None
+		'''
 		self.request_url = self.base_url
 
 	#Helper Methods for Building API Request URL(applicable to a single origin-destination pair)
@@ -54,10 +69,10 @@ class Tour:
 		'''
 
 		city_state_array = city_state_string.split(',')
-		return tuple(city_state_array)
+		return tuple([city_state_array[0].strip(),city_state_array[1].strip()])
 
 	def add_origin_to_base_url(self,tour_stop):
-		'''
+		'''Adds the name of origin tour-stop to the API request URL
 		Precondition: 	The input is in the form of a two-membered tuple
 		Postcondition: 	The request_url string instance variable has a query parameter for origin city embedded in the url
 		:param 		tour_stop: a tuple containing string elements, one for the city name, the other for the state name
@@ -70,7 +85,7 @@ class Tour:
 		self.request_url += tour_stop[1].strip()
 
 	def add_destination_to_base_url(self,tour_stop):
-		'''
+		'''Adds the tour-stop destination to the API-request URL
 		Precondition: 	The input is in the form of a two-membered tuple, and the request url already has origin query
 						parameter embedded in it
 		Postcondition: 	The request_url string instance variable has a query parameter for origin and destination cities
@@ -86,14 +101,24 @@ class Tour:
 		self.request_url += tour_stop[1].strip()
 
 	def add_mode_to_base_url(self,mode = "driving"):
+		'''Adds the traveling mode the API-request URL
+
+		:param 	mode: The possible traveling modes include driving, bicycling, and walking
+		:return: None
+		'''
 		if mode not in self.modes:
 			print("That mode is not valid") #change this implementation to raise a ValueError exception
 		else:
 			self.request_url += "&mode=" + mode.strip()
 
-	def add_sensor_to_base_url(self,hasCensor = False):
+	def add_sensor_to_base_url(self,hasSensor = False):
+		'''Adds the sensor parameter to the query string embedded in the API-request URL
+
+		:param hasSensor: True if sensor=True, False if sensor=False in query URL-query string
+		:return:
+		'''
 		self.request_url += "&sensor="
-		if not hasCensor:
+		if not hasSensor:
 			self.request_url += "false"
 		else:
 			self.request_url +="true"
@@ -103,6 +128,14 @@ class Tour:
 		return self.request_url
 
 	def __add__(self, other):
+		'''
+		Arithmetic addition operator is overloaded so that the locations list in the the two operands can be concatenated.
+		The locations list in the right-hand operand are concatenated to the end of the locations list in the left-hand operand
+
+
+		:param other:
+		:return:
+		'''
 		if(isinstance(other,Tour)):
 			return self.locations + other.locations
 		elif(isinstance(other,str)):
@@ -111,6 +144,18 @@ class Tour:
 			return NotImplemented
 
 	def __gt__(self, other):
+		'''Greater than operator is overloaded so that if the total distance of one tour is greater than that of
+		another, the result is True and False if otherwise
+
+		>>> t1 = Tour('New York,NY','Los Angeles,CA')
+		>>> t2 = Tour('New York,NY','Chicago,IL')
+		>>> t1 > t2
+		True
+
+
+		:param other: Another tour object whose total distance is being compared to the current tour object
+		:return:
+		'''
 		if(isinstance(other, Tour)):
 			return self.total_distance > other.total_distance
 		elif(isinstance(other, (int,float))):
@@ -119,6 +164,23 @@ class Tour:
 			return NotImplemented
 
 	def __ge__(self, other):
+		'''The >= operator is overloaded so that the total distance of one tour object can be compared to that of another
+
+		>>> t1 = Tour("New York, NY","Chicago, IL")
+		>>> t2 = Tour("New York, NY","Chicago,IL")
+		>>> t3 = Tour("New York, NY","Buffalo, NY")
+		>>> t1 >= t2
+		True
+		>>> t1 >= t3
+		True
+		>>> t3 >= t1
+		False
+
+
+
+		:param other:
+		:return:
+		'''
 		if(isinstance(other, Tour)):
 			return self.total_distance >= other.total_distance
 		elif(isinstance(other, (int,float))):
@@ -127,10 +189,38 @@ class Tour:
 			return NotImplemented
 
 	def __eq__(self, other):
+		'''The equality operator is overloaded such that two tour objects are equal if and only if their respective
+		tour stop locations are the same and in the same order
+
+		>>> t1 = Tour("New York,NY","Chicago, IL","Los Angeles,CA")
+		>>> t2 = Tour("New York,NY"," Chicago,IL","Los Angeles,CA")
+		>>> t3 = Tour("Los Angeles, CA", "Chicago,IL","New York,NY")
+		>>> t5 = Tour("New York,NY","Chicago,IL","Los Angeles,CA","San Francisco,CA")
+		>>> t1 == t2
+		True
+		>>> t1 == t3
+		False
+		>>> t1 == t5
+		False
+
+		:param other:
+		:return:
+		'''
 		if(len(self.locations) != len(other.locations)):
 			return False
 
-		for loc1,loc2 in zip(self.locations, other.locations):
+		city_state_list = []
+		other_city_state_list = []
+
+		for loc in self.locations:
+			city_state_tuple = self.parse_city_state_string(loc)
+			city_state_list.append(city_state_tuple)
+
+		for loc in other.locations:
+			city_state_tuple = other.parse_city_state_string(loc)
+			other_city_state_list.append(city_state_tuple)
+
+		for loc1,loc2 in zip(city_state_list, other_city_state_list):
 			if loc1 != loc2:
 				return False
 		return True
@@ -139,6 +229,19 @@ class Tour:
 		return self != other
 
 	def __lt__(self, other):
+		'''The arithmetic less than (<) operator is overloaded such that the less than comparison yields True if
+		the total distance for one tour object is less than the total distance of another object
+
+		>>> t1 = Tour("New York, NY", "Chicago, IL")
+		>>> t2 = Tour("New York, NY", "Boston, MA")
+		>>> t2 < t1
+		True
+		>>> t2 > t1
+		False
+
+		:param other:
+		:return:
+		'''
 		if (isinstance(other, Tour)):
 			return self.total_distance < other.total_distance
 		elif (isinstance(other, (int, float))):
@@ -147,6 +250,20 @@ class Tour:
 			return NotImplemented
 
 	def __le__(self, other):
+		'''The less than or equal (<=) arithmetic operator is overloaded such that a comparison yields True if the
+		total distance for one tour object is less than or equal to the tour distance for another tour object
+
+		>>> t1 = Tour("New York, NY","Chicago, IL")
+		>>> t2 = Tour("New York,NY", "Chicago,IL")
+		>>> t1 <= t2
+		True
+		>>> t3 = Tour("New York, NY","Los Angeles,LA")
+		>>> t1 <= t3
+		True
+
+		:param other:
+		:return:
+		'''
 		if (isinstance(other, Tour)):
 			return self.total_distance <= other.total_distance
 		elif (isinstance(other, (int, float))):
@@ -197,6 +314,14 @@ class Tour:
 			self.calculate_single_distance()
 			self.reset_request_URL()
 
+	def get_average_distance_between_stops(self):
+		'''Calculates the average distance between stops on the tour
+
+
+		:return: The mean distance among the set of consecutive distances between tour stops
+		'''
+
+		return statistics.mean(self.distances)
 
 	def get_total_distance(self):
 		return sum(self.distances)
@@ -206,6 +331,8 @@ class Tour:
 
 
 if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
 
 
 	t1 = Tour("New York, NY", "Lansing, MI", "Chicago, IL", "Houston, TX")
